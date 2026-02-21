b70a7169eb4e294a8a6f2bd8a2fbce81d9bdc341
Sadonna Liu <sadonnaliu@gmail.com>
2026-02-21 15:44:55 -0800

    testing-sado

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -32,8 +32,6 @@ export default function Contact() {
         </Container>
       </Container>
       <ContactSection />
-
-
     </>
   );
 }
--- app/studentInfo/page.tsx
+++ app/studentInfo/page.tsx
@@ -1,12 +1,22 @@
 /* This is the student info page */
+'use client';
+
+import React from "react";
+
+import { Box, AppBar, Toolbar, Typography, Container, Button, List, ListItem, ListItemButton
+,ListItemText,IconButton} from '@mui/material'
+
+import InfoCard from "../components/FloodingInfoCard";
 
 export default function StudentInfo(){
     <>
+    return( <Typography variant="h2" component="h1" fontWeight='bold' gutterBottom>
+              Contact Us
+            </Typography>)
     
-    {/* STUFF GOES HERE */ }
-    
+  
     </>
-    
-    
-    
+
     }
+
+
