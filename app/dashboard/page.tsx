c432296256832b48a76bfee0d41f6e315d865dd3
Marinah <maaweenah@gmail.com>
2026-03-07 04:06:02 -0800

    working on implementing RBAC to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,24 +2,29 @@
 
 'use client';
 import { Container, Typography, Box, Grid, Paper, Avatar } from '@mui/material';
+import { useAuth } from '../context/AuthContext';
 
 export default function Dashboard() {
+    const { user } = useAuth();
+
     return (
         <>
             <Container maxWidth="lg" disableGutters>
 
-                <Container maxWidth="lg">
-
                     <Box sx={{ display: 'flex', alignItems: 'center', gap: 6, mt: 4 }}>
 
                         <Typography variant='h4' sx={{ fontWeight: 'bold', color: 'black', mb: '1' }}>
-                            Welcome Back!
+                            Welcome Back, {user?.name}.
+
+                            {user?.role == 'admin' && <Typography> Admin based content </Typography>}
+                            {user?.role == 'tutor' && <Typography> Tutor based content </Typography>}
+                            {user?.role == 'student' && <Typography> Student based content </Typography>}
                         </Typography>
                     </Box>
 
                 </Container>
 
-            </Container>
+    
 
         </>
     );
